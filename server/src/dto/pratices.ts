55578a5cba002277a85c23275e4b73c768c8756a
Fernando Vedoin Garcia <fernando.vgarcia@hotmail.com>
2020-11-15 14:05:55 -0300

    Pratices model done.

--- server/src/dto/pratices.ts
+++ server/src/dto/pratices.ts
@@ -2,7 +2,227 @@ const pratices = [
     {
         type: 'architecture modeling',
         name: 'initial agile architectural modelling'
-    }
+    },
+    {
+        type: 'architecture modeling',
+        name: 'initial agile requirements modelling'
+    },
+    {
+        type: 'architecture modeling',
+        name: 'evolutionary design'
+    },
+    {
+        type: 'architecture modeling',
+        name: 'proved architecture early'
+    },
+    {
+        type: 'traditional analysis',
+        name: 'gantt chart details'
+    },
+    {
+        type: 'architecture modeling',
+        name: 'gantt chart high-level'
+    },
+    {
+        type: 'architecture modeling',
+        name: 'architecture specification detailed'
+    },
+    {
+        type: 'architecture modeling',
+        name: 'proved architecture early'
+    },
+    {
+        type: 'process/governance',
+        name: 'burn down chart'
+    },
+    {
+        type: 'process/governance',
+        name: 'velocity'
+    },
+    {
+        type: 'process/governance',
+        name: 'planning game'
+    },
+    {
+        type: 'process/governance',
+        name: 'daily stand up meeting'
+    },
+    {
+        type: 'process/governance',
+        name: 'iteration task list'
+    },
+    {
+        type: 'process/governance',
+        name: 'regular status report'
+    },
+    {
+        type: 'process/governance',
+        name: 'defect trend metrics'
+    },
+    {
+        type: 'database pratices',
+        name: 'continuous database integrations'
+    },
+    {
+        type: 'database pratices',
+        name: 'database testing'
+    },
+    {
+        type: 'database pratices',
+        name: 'database refactoring'
+    },
+    {
+        type: 'database pratices',
+        name: 'data naming conventions'
+    },
+    {
+        type: 'communication (team) - whiteboard practices',
+        name: 'whiteboard sketches'
+    },
+    {
+        type: 'communication (team) - whiteboard practices',
+        name: 'whiteboard sketching modelling'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'continuous code integration'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'test driven development'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'code refactoring'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'developer tests'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'flexible architeture'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'evolutionary design'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'simple design'
+    },
+    {
+        type: 'agile quality assurance',
+        name: 'collectve ownersip'
+    },
+    {
+        type: 'communication (team)',
+        name: 'paper based modelling'
+    },
+    {
+        type: 'communication (team)',
+        name: 'paper models'
+    },
+    {
+        type: 'communication (team)',
+        name: 'pair programming'
+    },
+    {
+        type: 'code analysis and inspection',
+        name: 'static code analysis'
+    },
+    {
+        type: 'code analysis and inspection',
+        name: 'code inspection'
+    },
+    {
+        type: 'lightweight testing and review',
+        name: 'independent confirmatory exploratory testing'
+    },
+    {
+        type: 'lightweight testing and review',
+        name: 'customer acceptance tests'
+    }, 
+    {
+        type: 'lightweight testing and review',
+        name: 'model document reviews'
+    },
+    {
+        type: 'architecture and configuration',
+        name: 'architeture specification high-level'
+    },
+    {
+        type: 'architecture and configuration',
+        name: 'configuration management'
+    },
+    {
+        type: 'architecture and configuration',
+        name: 'architeture specification detailed'
+    },
+    {
+        type: 'traditional quality assurance',
+        name: 'test plan'
+    },
+    {
+        type: 'traditional quality assurance',
+        name: 'source code'
+    },
+    {
+        type: 'traditional quality assurance',
+        name: 'defect reports'
+    },
+    {
+        type: 'traditional quality assurance',
+        name: 'regular status report'
+    },
+    {
+        type: 'coding standards',
+        name: 'coding standard'
+    },
+    {
+        type: 'coding standards',
+        name: 'data naming conventions'
+    },
+    {
+        type: 'lightweight requirements',
+        name: 'requirements specification high-level'
+    },
+    {
+        type: 'lightweight requirements',
+        name: 'use cases light'
+    },
+    {
+        type: 'incremental and iterative development',
+        name: 'incremental delivery'
+    },
+    {
+        type: 'incremental and iterative development',
+        name: 'small releases'
+    },
+    {
+        type: 'incremental and iterative development',
+        name: 'iterative development'
+    },
+    {
+        type: 'incremental and iterative development',
+        name: 'sustainable pace'
+    },
+    {
+        type: 'incremental and iterative development',
+        name: 'active stakeholder participation'
+    },
+    {
+        type: 'incremental and iterative development',
+        name: 'working demoable software'
+    },
+    {
+        type: 'communication (customers)',
+        name: 'co located team'
+    },
+    {
+        type: 'communication (customers)',
+        name: 'active stakeholder participation'
+    },
 ];
 
 export default pratices;
