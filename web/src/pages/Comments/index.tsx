7ffc84dfd5e68bee9abc514c8a1724dbfa4c9483
Fernando <fernando.vgarcia@hotmail.com>
2020-12-27 02:03:18 -0300

    Listing comments.

--- web/src/pages/Comments/index.tsx
+++ web/src/pages/Comments/index.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 
 import api from '../../services/api';
 
+import './styles.css';
+
 function Comments() {
     const { id } = useParams();
 
@@ -10,10 +12,16 @@ function Comments() {
 
     const [comments, setComments] = useState<any[]>([]);
 
+    useEffect(() => {
+        api.get(`relation/${id}/comments`).then(response => {
+            setComments(response.data);
+        });
+    }, []);
+
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
-        await api.post(`comments`, {
+        await api.post('comments', {
             comment, project_risk_pratice_id: id
         }).then(response => {
             window.location.reload();
@@ -24,15 +32,29 @@ function Comments() {
 
     return (
         <div id="page-project-pratices">
-            <form onSubmit={handleSubmit}>
-                <textarea
-                    className="form-input"
-                    placeholder="Comment"
-                    value={comment}
-                    onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setComment(e.currentTarget.value)}
-                />
-                <button id="submit" type="submit">Save</button>
-            </form>
+            <h1 id="page-title">Comments</h1>
+            <section className="comment-sections">
+                {comments.map(item => (
+                    <div key={item.id} className="comment-group">
+                        <p className="comment-date">[{new Date(item.commented_on).toDateString()}]</p>
+                        <div>
+                            <p className="comment-author">{item.name}</p>
+                            <p>{item.comment}</p>
+                        </div>
+                    </div>
+                ))}
+            </section>
+            <section className="comment-sections">
+                <form onSubmit={handleSubmit}>
+                    <textarea
+                        className="form-input"
+                        placeholder="Comment"
+                        value={comment}
+                        onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setComment(e.currentTarget.value)}
+                    />
+                    <button id="submit" type="submit">Save</button>
+                </form>
+            </section>
         </div>
     );
 }
